8a3fcb938aa04d89f28ee8e15179a907bc21d1e2
fmitei <faith.mitei@trustgro.com>
2023-02-13 22:25:57 +0300

    first commit

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+  const handleFocus = (e) => {
+    console.log("Good!", e.target);
+  };
+
+  return (
+    <div>
+      <button
+        onFocus={handleFocus}
+        onBlur={(e) => {
+          console.log("Hey! Eyes on me!", e.target);
+        }}
+      >
+        Eyes on me
+      </button>
+    </div>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,15 @@
-// Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  const handleChange = (event) => {
+    console.log("Entering password", event.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" onChange={handleChange}></input>
+    </div>
+  );
 }
 
 export default Keypad;
